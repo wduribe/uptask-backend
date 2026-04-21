90b42b1bb56a59a289348d64c539ea4b8d961528
wilder ub30 <urbina153@gmail.com>
2026-04-20 19:23:43 -0500

    version 2

--- src/presentation/middlewares/projects.middleware.ts
+++ src/presentation/middlewares/projects.middleware.ts
@@ -11,18 +11,18 @@ declare global {
 }
 
 export const validateProjectExist = async (req: Request, res: Response, next: NextFunction) => {
-    try{
-        if(!isValidMongoId(req.params.projectId)) return res.status(400).json({error: 'El id del proyecto es inválido'});
+    try {
+        if (!isValidMongoId(`${req.params.projectId}`)) return res.status(400).json({ error: 'El id del proyecto es inválido' });
 
         const project = await ProjectModel.findById(req.params.projectId);
-        if(!project) return res.status(400).json({error: 'Proyecto no encontrado'});
+        if (!project) return res.status(400).json({ error: 'Proyecto no encontrado' });
 
         //* New Property
         req.project = project;
 
         next();
 
-    }catch(error){
-        res.status(500).json({error: 'Error interno del servidor'});
+    } catch (error) {
+        res.status(500).json({ error: 'Error interno del servidor' });
     }
 }
