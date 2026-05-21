5ddf1159137359131d118bb59c7ea34878499b93
wilder ub30 <urbina153@gmail.com>
2026-05-20 22:24:24 -0500

    Configuracion email

--- src/config/nodemailer.ts
+++ src/config/nodemailer.ts
@@ -5,11 +5,16 @@ const { MAILER_SECRET_KEY, MAILER_EMAIL, MAILER_SERVICE } = envsAdapter;
 
 const config = () => {
     return {
+        host: "smtp.gmail.com",
+        port: 587,
+        secure: false,
         service: MAILER_SERVICE,
         auth: {
             user: MAILER_EMAIL,
             pass: MAILER_SECRET_KEY
-        }
+        },
+        connectionTimeout: 10000,
+        socketTimeout: 10000
     }
 }
 
--- src/presentation/services/authSendEmail.service.ts
+++ src/presentation/services/authSendEmail.service.ts
@@ -8,36 +8,51 @@ interface IEmail {
 
 export class AuthSendEmailService {
 
-    sendConfirmationEmail = async ({email, name, token}: IEmail) => {
-        const info = await transporter.sendMail({
-            from: 'UpTask <urbina153@gmail.com>',
-            to: email,
-            subject: 'UpTask - Confirma tu cuenta',
-            text: 'UpTask - Confirma tu cuenta',
-            html: `<p>Hola: ${name}, has creado tu cuenta en Uptask, ya casi esta todo listo, solo debes confirmar tu cuenta </p>
+    
+
+    sendConfirmationEmail = async ({ email, name, token }: IEmail) => {
+        try {
+            const info = await transporter.sendMail({
+                from: 'UpTask <urbina153@gmail.com>',
+                to: email,
+                subject: 'UpTask - Confirma tu cuenta',
+                text: 'UpTask - Confirma tu cuenta',
+                html: `<p>Hola: ${name}, has creado tu cuenta en Uptask, ya casi esta todo listo, solo debes confirmar tu cuenta </p>
                 <p>Visita el siguiente enlace</p>
                 <a href="${envsAdapter.FRONTED_URL}/public/confirm-account">Confirmar cuenta</a>
                 <p>E ingresa el código: <b>${token}</b></p>
                 <p>Este token expira en 10 minutos</p>   
             `
-        });
+            });
 
-     }
+            return true;
+        } catch (error) {
+            return false;
+        }
 
-     sendPasswordResetToken = async ({email, name, token}: IEmail) => {
-        const info = await transporter.sendMail({
-            from: 'UpTask <urbina153@gmail.com>',
-            to: email,
-            subject: 'UpTask - Reestablece tu contraseña',
-            text: 'UpTask - Reestablece tu contraseña',
-            html: `<p>Hola: ${name}, has solicitado cambiar tu contraseña </p>
+    }
+
+    sendPasswordResetToken = async ({ email, name, token }: IEmail) => {
+        try {
+            const info = await transporter.sendMail({
+                from: 'UpTask <urbina153@gmail.com>',
+                to: email,
+                subject: 'UpTask - Reestablece tu contraseña',
+                text: 'UpTask - Reestablece tu contraseña',
+                html: `<p>Hola: ${name}, has solicitado cambiar tu contraseña </p>
                 <p>Visita el siguiente enlace</p>
                 <a href="${envsAdapter.FRONTED_URL}/public/set-new-password">Reestablecer contraseña</a>
                 <p>E ingresa el código: <b>${token}</b></p>
                 <p>Este token expira en 10 minutos</p>   
             `
-        });
+            });
 
-     }
+
+            return true;
+        } catch (error) {
+            return false;
+        }
+
+    }
 
 }
